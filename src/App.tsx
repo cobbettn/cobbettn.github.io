30c696f2a40ad51c0e05a8da2dfe4e94dcd066bc
Nathan Cobbett <cobbett.n@gmail.com>
2023-03-22 16:05:21 -0400

    little better

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import TVFrame from './components/tv/TVFrame'
 import githubLogo from './assets/images/GitHub_Logo_White.png'
 import linkedinLogo from './assets/images/linkedin.png'
 import colorBars from './assets/images/SMPTE_Color_Bars.svg.png'
+import tvStatic from './assets/images/static.gif'
 
 function App() {
-  const totalChannels = 25;
+  // 5 x 5 grid
   const channels = [
     {
       name: 'github',
       image: githubLogo,
+      link: ''
     },
     {
       name: 'linkedin',
-      image: linkedinLogo
+      image: linkedinLogo,
+      link: ''
     },
     null,
     null,
@@ -48,7 +51,7 @@ function App() {
     <div className="grid-container">
       { channels.map((channel, i) => (
         <TVFrame key={`channel-${i}`}>
-          <img className='channelImage' style={{width: '20vh'}} src={channel?.image ?? colorBars} alt="" />
+          <img className='channelImage' style={{width: '20vh'}} src={channel?.image ?? tvStatic} alt="" />
         </TVFrame>
       ))}
     </div>
--- src/components/tv/TVFrame.tsx
+++ src/components/tv/TVFrame.tsx
@@ -1,17 +1,28 @@
 // import tvFrame from './assets/tv-frame.png';
+import { useState } from 'react';
 import tvFrame from '../../assets/images/tv-alpha500px.png'
 import { useWindowDimensions } from '../../hooks/useWindowSize';
 
 const TVFrame = ({ children }) => {
   // const { height, width } = useWindowDimensions
-  
+  const commonStyles = {position: 'absolute', width:'20vh', height:'20vh'}
+  const [glow, setGlow] = useState(false)
+
   return (
     <div className='grid-item'>
-      <img style={{position: 'absolute', width:'20vh', height:'20vh', zIndex:100}} 
+      <img
+        onMouseEnter={() => {
+          setGlow(true)
+        }}
+        onMouseLeave={() => {
+          setGlow(false)
+        }}
+        className='tv-content'
+        style={{zIndex: 100}} 
         src={tvFrame} 
         alt="Television Frame" />
-      <div style={{position: 'absolute', width:'20vh', height:'20vh'}}>
-        {children}
+      <div className='tv-content' style={{display: 'flex', filter: `brightness(${glow ? 1.33 : 1})`}} >
+        { children }
       </div>
     </div>
   );
