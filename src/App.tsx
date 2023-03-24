78df8a51cfa835c3458f1f6258e4d42091557042
Nathan Cobbett <cobbett.n@gmail.com>
2023-03-24 12:28:17 -0400

    remove div

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,6 @@ function App() {
 
   return (
     <div className="grid-container">
-      <div>hey bud</div>
       { 
         channels.map((channel, i) => (
           <TVFrame link={`${channel?.link}`} key={`channel-${i}`}>
