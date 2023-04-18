93ad5b50e4f6a3fe65505f87754beb962169e9cf
Nathan Cobbett <cobbett.n@gmail.com>
2023-04-18 15:28:12 -0400

    bio, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,8 @@ function App() {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: 'black',
+    color: 'white',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
@@ -102,12 +103,19 @@ function App() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="h6" component="h2" pb={2}>
             About me
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Bio goes here...
-          </Typography>
+          <Typography component="p" pb={2}>
+            I like to build things with web technologies. 
+            I started programming as a hobby in my downtime and it eventually turned into a career.
+            Through my work as a software engineer I strive to create robust, accessible and performant applications that delight and empower users.
+            I'm open to job opportunites where I can solve interesting problems and further expand my skill set as an engineer.
+          </Typography>   
+          <Typography component="p">
+            When I'm not on my computer I like spending time in the woods, retro gaming and bouldering.
+            I also enjoy taking pictures as a hobby and help run a small photography business. 
+          </Typography>       
         </Box>
       </Modal>
     </>
