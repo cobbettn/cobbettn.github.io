725eede78117963db9b01c5479f850eacacb756a
Nathan Cobbett <cobbett.n@gmail.com>
2025-07-20 21:31:20 -0400

    new channels

--- src/data.ts
+++ src/data.ts
@@ -8,6 +8,8 @@ import inat from './assets/images/inat.jpg'
 import steam from  './assets/images/steam.png'
 import wordpress from './assets/images/wordpress.png'
 import logic from './assets/images/logic.jpg'
+import neocities from './assets/images/neocities.png'
+import shroom from './assets/images/shroom.jpg'
 
 import Resume from './assets/Resume.pdf'
 
@@ -48,13 +50,39 @@ export const channels: (IChannel | undefined)[] = [
     link: 'https://blog.ncobbett.com'
   },
   {
+    name: 'code',
+    image: codeIcon,
+    link: 'https://github.com/cobbettn/cobbettn.github.io',
+    description: 'github repository for this website'
+  },
+
+  // row 2
+  {
+    name: 'resume',
+    description: 'resume',
+    image: resumeIcon,
+    link: Resume,
+  },
+  {
     name: 'linkedin',
     description: 'linkedin',
     image: linkedinLogo,
     link: 'https://www.linkedin.com/in/nathan-cobbett/',
   },
+  {
+    name: 'inaturalist',
+    description: 'inaturalist',
+    image: inat,
+    link: "https://www.inaturalist.org/people/skatepunk"
+  },
+  {
+    name: 'neocities',
+    description: 'neocities',
+    image: neocities,
+    link: 'https://ncobbett.neocities.org/'
+  },
 
-  // row 2
+  // row 3
   {
     name: 'steam',
     description: 'steam',
@@ -62,29 +90,9 @@ export const channels: (IChannel | undefined)[] = [
     link: "https://steamcommunity.com/profiles/76561198262196266/"
   },
   {
-    name: 'code',
-    image: codeIcon,
-    link: 'https://github.com/cobbettn/cobbettn.github.io',
-    description: 'github repository for this website'
-  },
-    {
-    name: 'inaturalist',
-    description: 'inaturalist',
-    image: inat,
-    link: "https://www.inaturalist.org/people/skatepunk"
-  },
-  {
-    name: 'resume',
-    description: 'resume',
-    image: resumeIcon,
-    link: Resume,
-  },
-
-  // row 3
-  {
     name: 'mushroom',
     description: 'mushroom',
-    image: colorBars,
+    image: shroom,
     view: Channels.mushroom
   },
   {
@@ -93,7 +101,6 @@ export const channels: (IChannel | undefined)[] = [
     image: logic,
     link: 'https://ncobbett.com/TruthTables'
   },
-  undefined,
   undefined,
 
   // row 4
